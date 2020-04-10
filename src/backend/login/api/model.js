22161ba58df250a4505e92358ff0ed16e10c0df6
Luis Gallego Quero <lgaq94@gmail.com>
2020-04-10 11:57:12 +0200

    modelo inicial. #27

---
+++ src/backend/login/api/model.js
@@ -0,0 +1,49 @@
+'use strict';
+
+let mongoose = require('mongoose');
+
+
+/**
+ * @description User MongoDB Schema
+ *
+ */
+let userSchema = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.ObjectId,
+        default: mongoose.Types.ObjectId()
+    },
+    user_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String
+    },
+    rol: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date
+    }
+}, {
+    versionKey: false
+});
+
+
+module.exports = mongoose.model('user', userSchema);

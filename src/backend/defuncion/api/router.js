051bbfc2ea8c5b020d89b710849247146081fe4d
Luis Gallego Quero <lgaq94@gmail.com>
2020-04-25 11:39:09 +0200

    router corregido. #44

--- src/backend/defuncion/api/router.js
+++ src/backend/defuncion/api/router.js
@@ -5,39 +5,47 @@ let router = new express.Router();  // express router
 
 let difuntoController = require('./difunto_controller');
 let servicioController = require('./servicio_controller');
-let controller = require('./controller');
+let controller = require('./defuncion_controller');
 
-// Trabajan con la info de difunto y servicio en conjunto
+// /DEFUNCION/ => Trabajan con la info de difunto y servicio en conjunto
 router.get('/', controller.status);
+// Debe crear el enlace difunto - servicio
 router.post('/', controller.create);
+// Debe permitir modificar ambos, pero
 router.put('/', controller.update);
 router.get('/list', controller.list);
 
+// /DEFUNCION/DIFUNTO
 router.get('/difunto', difuntoController.status);
 router.post('/difunto', difuntoController.create);
 // Debe añadir el ID de un usuario como updater
 router.put('/difunto', difuntoController.update);
 router.get('/difunto/list', difuntoController.list);
 
-router.get('difunto/cliente', difuntoController.getNoClientes);
-router.get('difunto/familiar', difuntoController.getNoFamilia);
-router.get('difunto/factura', difuntoController.getNoFactura);
-router.post('difunto/cliente', difuntoController.asignarCliente);
-router.post('difunto/familia', difuntoController.asignarFamilia);
-router.post('difunto/factura', difuntoController.asignarFactura);
+// Para las diferentes asignaciones al difunto
+router.get('/difunto/cliente', difuntoController.getNoClientes);
+router.get('/difunto/familia', difuntoController.getNoFamilia);
+router.get('/difunto/factura', difuntoController.getNoFactura);
+router.post('/difunto/cliente', difuntoController.asignarCliente);
+router.post('/difunto/familia', difuntoController.asignarFamilia);
+router.post('/difunto/factura', difuntoController.asignarFactura);
 
+// /DEFUNCION/SERVICIO
 router.get('/servicio', servicioController.status);
 router.post('/servicio', servicioController.create);
 router.put('/servicio', servicioController.update);
 router.get('/servicio/list', servicioController.list);
 
+// /DEFUNCION/DIFUNTO
+router.get('/difunto/:_id', difuntoController.read);
+router.delete('/difunto/:_id', difuntoController.destroy);
+
+// /DEFUNCION/
 router.get('/:_id', controller.read);
 // Solo si el usuario es 'Administrador' puede borrar.
 router.delete('/:_id', controller.destroy);
 
-router.get('/difunto/:_id', controller.read);
-router.delete('/difunto/:_id', controller.destroy);
-
+// /DEFUNCION/SERVICIO
 router.get('/servicio/:_id', controller.read);
 router.delete('/servicio/:_id', controller.destroy);
 

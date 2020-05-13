7b243088225a820e921a0b792b936856015d389a
Luis Gallego Quero <lgaq94@gmail.com>
2020-05-13 23:00:53 +0200

    nuevos tests listos. close #78

--- src/backend/log/test/test_log.js
+++ src/backend/log/test/test_log.js
@@ -5,11 +5,19 @@ let request = require('supertest'),
     moment = require('moment'),
     app = require('../');
 
+let logModel = require('../api/log_model');
+
 describe('LOG API:', () => {
+
+    /* Limpiar la db */
+    before(done => {
+        logModel.remove({})
+            .then(() => done())
+    });
 
     describe('POST /log', () => {
 
-        it('Debe recibir y guardar un nuevo log', done => {
+        it('Debe guardar el nuevo log', done => {
             request(app)
                 .post('/log')
                 .send({
@@ -27,7 +35,7 @@ describe('LOG API:', () => {
                         'output2': '44'
                     }
                 })
-                .expect(200, done)
+                .expect(204, done)
         });
 
         it('Si la peticion es vacia, debe devolver error', done => {
@@ -45,4 +53,40 @@ describe('LOG API:', () => {
         });
     });
 
+    describe('GET /log/:servicio', () => {
+
+        it('Debe devolver los logs del servicio', done => {
+            request(app)
+                .get('/log/0')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if(err) done(err);
+                    else {
+                        let result = res.body;
+                        expect(result[0].service).to.equal('DEFUNCION');
+                        expect(result[0].method).to.equal('GET');
+                        expect(result[0].route).to.equal('/ruta');
+                        expect(result[0].status).to.equal(200);
+                        expect(result[0].input.input1).to.equal('11');
+                        expect(result[0].output.output2).to.equal('44');
+                        done();
+                    }
+                });
+        });
+
+        it('Si no hay logs para ese servicio, devuelve vacio ', done => {
+            request(app)
+                .get('/log/1')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end((err, res) => {
+                    if(err) done(err);
+                    else {
+                        expect(res.body).to.have.lengthOf(0);
+                        done();
+                    }
+                });
+        });
+    });
 });
